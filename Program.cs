d31f2700b03ea4aed960d4a0dc5eedb38a3ae8dc
ShaunMBennett <89603464+ShaunMBennett@users.noreply.github.com>
2022-02-09 14:00:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+//Name: Shaun Bennett
+//Course: CSCI 3110- Advanced Web Development
+//Date Created: 2/5/2022
+//Sources: https://docs.microsoft.com/en-us/dotnet/csharp/ and Powerpoint for C# refresher
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
